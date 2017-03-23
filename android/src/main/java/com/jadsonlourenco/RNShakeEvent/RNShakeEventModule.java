dbf626c29d45aef4f8b00f1ee4b040155c8e6c6e
LittleKey <LittleKeyRain@gmail.com>
2017-03-23 11:28:52 +0800

    fix android module typo

--- android/src/main/java/com/jadsonlourenco/RNShakeEvent/RNShakeEventModule.java
+++ android/src/main/java/com/jadsonlourenco/RNShakeEvent/RNShakeEventModule.java
@@ -19,7 +19,7 @@ public class RNShakeEventModule extends ReactContextBaseJavaModule implements Sh
         super(reactContext);
         SensorManager sensorManager = (SensorManager) reactContext.getSystemService(Context.SENSOR_SERVICE);
         ShakeDetector shakeDetector = new ShakeDetector(this);
-        ShakeDetector.setSensitivity(30);
+        shakeDetector.setSensitivity(30);
         shakeDetector.start(sensorManager);
     }
 
